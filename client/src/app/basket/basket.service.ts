eb4c05441e4c56195b9e3fcfb171d56db5337848
Stephen Smith <stephensmith_aus@yahoo.com.au>
2022-10-03 08:11:48 +1100

    End of Section 14

---
+++ client/src/app/basket/basket.service.ts
@@ -0,0 +1,165 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Basket, IBasket, IBasketItem, IBasketTotals } from '../shared/models/basket';
+import { IProduct } from '../shared/models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasketService {
+  readonly LocalStorageBasketIdKey = 'basket_id';
+
+  baseUrl = environment.apiUrl;
+  private basketSource = new BehaviorSubject<IBasket>(null);
+  basket$ = this.basketSource.asObservable();
+  private basketTotalSource = new BehaviorSubject<IBasketTotals>(null);
+  basketTotal$ = this.basketTotalSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  get localStorageBasketId(): string {
+    return localStorage.getItem(this.LocalStorageBasketIdKey);
+  }
+
+  getBasket(id: string) {
+    const url = `${this.baseUrl}basket?id=${id}`;
+
+    return this.http
+      .get<IBasket>(url)
+      .pipe(
+        map((basket: IBasket) => {
+          this.basketSource.next(basket);
+          this.calculateTotals();
+        }),
+      );
+  }
+
+  setBasket(basket: IBasket) {
+    const url = `${this.baseUrl}basket`;
+
+    return this.http
+      .post<IBasket>(url, basket)
+      .subscribe((response: IBasket) => {
+        this.basketSource.next(response);
+        this.calculateTotals();
+      }, error => {
+        console.error(error);
+      });
+  }
+
+  getCurrentBasketValue(): IBasket {
+    return this.basketSource.value;
+  }
+
+  addItemToBasket(
+    item: IProduct,
+    quantity = 1
+  ) {
+    const itemToAdd: IBasketItem 
+      = this.mapItemToBasketItems(item, quantity);
+    const basket = this.getCurrentBasketValue()
+      ?? this.createBasket();
+    basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
+    this.setBasket(basket);
+  }
+
+  incrementItemQuantity(item: IBasketItem) {
+    const basket = this.getCurrentBasketValue();
+    const foundItemIndex = basket.items.findIndex(x => x.id === item.id);
+    if (foundItemIndex < 0) return;
+
+    basket.items[foundItemIndex].quantity++;
+    this.setBasket(basket);
+  }
+
+  decrementItemQuantity(item: IBasketItem) {
+    const basket = this.getCurrentBasketValue();
+    const foundItemIndex = basket.items.findIndex(x => x.id === item.id);
+    if (foundItemIndex < 0) return;
+
+    const basketItem = basket.items[foundItemIndex];
+    if (basketItem.quantity > 1) {
+      basketItem.quantity--;
+      this.setBasket(basket);
+    } else {
+      this.removeItemFromBasket(item);
+    }
+  }
+
+  removeItemFromBasket(item: IBasketItem) {
+    const basket = this.getCurrentBasketValue();
+    if (!basket.items.some(x => x.id === item.id)) return;
+
+    basket.items = basket.items.filter(i => i.id !== item.id);
+    if (basket.items.length > 0) {
+      this.setBasket(basket);
+    } else {
+      this.deleteBasket(basket);
+    }
+  }
+
+  deleteBasket(basket: IBasket) {
+    const url = `${this.baseUrl}basket?id=${basket.id}`;
+
+    return this.http
+      .delete(url)
+      .subscribe(() => {
+        this.basketSource.next(null);
+        this.basketTotalSource.next(null);
+        localStorage.removeItem(this.LocalStorageBasketIdKey);
+      })
+  }
+
+  private addOrUpdateItem(
+    items: IBasketItem[], 
+    itemToAdd: IBasketItem, 
+    quantity: number): IBasketItem[] {
+    console.log('items: IBasketItem[]', items);
+    const index = items.findIndex(i => i.id === itemToAdd.id);
+    if (index === -1) {
+      itemToAdd.quantity = quantity;
+      items.push(itemToAdd);
+    } else {
+      items[index].quantity += quantity;
+    }
+
+    return items;
+  }
+
+  private createBasket(): IBasket {
+    const basket = new Basket();
+    localStorage.setItem(this.localStorageBasketId, basket.id);
+
+    return basket;
+  }
+
+  private mapItemToBasketItems(item: IProduct, quantity: number): IBasketItem {
+    const basketItem: IBasketItem = {
+      id: item.id,
+      productName: item.name,
+      price: item.price,
+      pictureUrl: item.pictureUrl,
+      quantity,
+      brand: item.productBrand,
+      type: item.productType,
+    };
+
+    return basketItem;
+  }
+
+  private calculateTotals() {
+    const basket = this.getCurrentBasketValue();
+    const shipping = 0;
+    const subtotal = basket.items.reduce((a, b) => 
+      (b.price * b.quantity) + a, 0);
+    const total = subtotal + shipping;
+    const basketTotals: IBasketTotals = {
+      shipping,
+      subtotal,
+      total,
+    };
+    this.basketTotalSource.next(basketTotals);
+  }
+}
--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
@@ -10,18 +11,35 @@ import { ShopService } from '../shop.service';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
-  @Input() product: IProduct;
+  product: IProduct;
+  quantity = 1;
 
   constructor(
     private shopService: ShopService,
     private activatedRoute: ActivatedRoute,
     private breadcrumb: BreadcrumbService,
+    private basketService: BasketService,
   ) { 
     this.setBreadcrumbName();
   }
 
   ngOnInit(): void {
     this.loadProduct();
+  }
+
+  addItemToBasket() {
+    this.basketService.addItemToBasket(
+      this.product, this.quantity);
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity <= 1) return;
+
+    this.quantity--;
   }
 
   private loadProduct() {
